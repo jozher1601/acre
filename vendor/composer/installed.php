7d4f76789ee59a4a25906db67e20a2a1e831d3a0
Jose Hernández <joseluis1601hr@gmail.com>
2022-04-19 23:28:54 -0600

    Frameworks

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '2f6726295e6d1689822bdfcb54f47b0672139071',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2f6726295e6d1689822bdfcb54f47b0672139071',
+    ),
+    'kumbia/framework' => 
+    array (
+      'pretty_version' => 'v1.1.5',
+      'version' => '1.1.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '72235c00af4a51300df17e1922ec27c4f40732b9',
+    ),
+  ),
+);
